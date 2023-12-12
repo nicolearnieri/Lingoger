fb03666d0ad1b5db72616985f34ca3eb467df783
nika <129058062+nicolearnieri@users.noreply.github.com>
2023-12-12 19:26:40 +0100

    EmailSender

---
+++ src/main/java/info/unical/Model/EmailSender.java
@@ -0,0 +1,32 @@
+package info.unical.Model;
+
+import org.simplejavamail.api.email.Email;
+import org.simplejavamail.api.mailer.Mailer;
+import org.simplejavamail.email.EmailBuilder;
+import org.simplejavamail.mailer.MailerBuilder;
+import org.simplejavamail.api.mailer.config.TransportStrategy;
+
+public class EmailSender {
+    private static final String username = "lingoger@libero.it";
+    private static final String password = "ProgettoLingoger31!";
+
+    public static void sendEmail(String recipient, String subject, String body) {
+        // Creazione dell'oggetto Email utilizzando Simple Java Mail
+        Email email = EmailBuilder.startingBlank()
+                .from(username) // In Gmail, l'indirizzo e-mail del mittente deve corrispondere al nome utente (username).
+                .to(recipient)
+                .withSubject(subject)
+                .withPlainText(body)
+                .buildEmail();
+
+        // Configurazione del Mailer per inviare l'e-mail utilizzando il server SMTP di Gmail
+        Mailer mailer = MailerBuilder.withSMTPServer("smtp.libero.it", 587, username, password)
+                .withTransportStrategy(TransportStrategy.SMTP_TLS) // Usa TLS per la crittografia
+                .buildMailer();
+
+        // Invio dell'e-mail
+        mailer.sendMail(email);
+
+        System.out.println("Email inviata con successo a " + recipient);
+    }
+}
