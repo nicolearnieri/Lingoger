ff096e6505b7d3292ff754ac58e7b03c891913b0
nika <129058062+nicolearnieri@users.noreply.github.com>
2023-12-13 11:12:28 +0100

    cose

---
+++ src/main/java/info/unical/Controller/StartController.java
@@ -0,0 +1,4 @@
+package info.unical.Controller;
+
+public class StartController {
+}
--- src/main/java/info/unical/View/SceneHandler.java
+++ src/main/java/info/unical/View/SceneHandler.java
@@ -1,8 +1,13 @@
 package info.unical.View;
 
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import java.util.List;
+import java.util.Objects;
 
 public class SceneHandler
 {
@@ -16,7 +21,65 @@ public class SceneHandler
     private Scene logInScene;
     private Stage stage;
 
+    private Stage logInOrSignUpStage;
+
     private String theme = "LingogerTheme";
-    private String font = "LingogerFont";
+    private String font = "FontPoppins";
+
+    private static SceneHandler instance = null;
+
+    private SceneHandler()
+    {}
+
+    public void init(Stage primaryStage) throws Exception { //metodo che puÃ² generare eccezione
+        if(stage != null) return;
+
+        stage = primaryStage;
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(FXML_PATH + "HomeInterface.fxml"));
+        scene = new Scene(loader.load(), 900, 700); //v:larghezza, v1:altezza
+        StartController controller= loader.getController();
+        changedTheme(scene);
+        stage.setTitle("DeliverBoo");
+        Image icon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/Icone/Ghost.png")));// Carica l'immagine dell'icona dall'URL relativo nel tuo progetto
+        stage.getIcons().add(icon); // Imposta l'icona per la finestra
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+
+    public void closeStage(Stage myStage)
+    {
+        myStage.close();
+    }
+
+    public void hideStage(Stage myStage){myStage.hide();}
+
+    public void showStage(Stage myStage){myStage.show();}
+
+    private void changedTheme(Scene scene) {
+        setCSSForScene(scene);
+        setCSSForAlert(alertError);
+        setCSSForAlert(alertInfo);
+        setCSSForAlert(alertConfirmation);
+    }
+
+
+
+    //aiuto
+    private void setCSSForScene(Scene scene) { //in base a theme setta i css per la scena
+        Objects.requireNonNull(scene);
+        scene.getStylesheets().clear();
+        for(String resource : resources)
+            scene.getStylesheets().add(resource);
+
+        if (font.equals("FontMontserrat")) {
+            loadMontserrat();
+        } else {
+            loadOpenDyslexic();
+        }
+    }
+
+
 
 }
