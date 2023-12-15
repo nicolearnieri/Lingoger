44ed88f11972ae6d9579af32c26f7fd378b8d049
nika <129058062+nicolearnieri@users.noreply.github.com>
2023-12-15 10:11:35 +0100

    Controllers have been set up with all the names

--- src/main/java/info/unical/Controller/LogInController.java
+++ src/main/java/info/unical/Controller/LogInController.java
@@ -1,4 +1,22 @@
 package info.unical.Controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class LogInController {
+
+    @FXML
+    private Button logButton;
+
+    @FXML
+    private TextField passwordField;
+
+    @FXML
+    private Label registrationCLabel;
+
+    @FXML
+    private TextField usernameOrEmailField;
+
 }
--- src/main/java/info/unical/Controller/SignUpController.java
+++ src/main/java/info/unical/Controller/SignUpController.java
@@ -1,4 +1,39 @@
 package info.unical.Controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class SignUpController {
+
+    @FXML
+    private TextField confirmPVisible;
+
+    @FXML
+    private PasswordField confirmPasswordField;
+
+    @FXML
+    private TextField emailLabel;
+
+    @FXML
+    private ChoiceBox<?> languageChooser;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Button registrationButton;
+
+    @FXML
+    private TextField seePField;
+
+    @FXML
+    private Label signUpClickableLabel;
+
+    @FXML
+    private TextField usernameLabel;
+
 }
