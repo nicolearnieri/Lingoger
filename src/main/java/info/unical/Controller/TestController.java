de67945fe3e737aa3f75db69f07116d9f85eeef1
nika <129058062+nicolearnieri@users.noreply.github.com>
2023-12-17 15:26:22 +0100

    forse è fatto? aiuto

--- src/main/java/info/unical/Controller/TestController.java
+++ src/main/java/info/unical/Controller/TestController.java
@@ -5,41 +5,37 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
-
-/* vengono usati due mediatori, uno che media tra le varie domande (TestMediator, si occupa del test in generale) e uno che
-media tra le varie risposte (QuestionsMediator, quindi si occupa di una singola domanda alla volta);
- */
 public class TestController {
 
     @FXML
     private Button confirmationButton;
 
     @FXML
-    private Button q1First;
+    private Button q1a1;
 
     @FXML
-    private Button q1Second;
+    private Button q1a2;
 
     @FXML
-    private Button q1Third;
+    private Button q1a3;
 
     @FXML
-    private Button q2First;
+    private Button q2a1;
 
     @FXML
-    private Button q2Second;
+    private Button q2a2;
 
     @FXML
-    private Button q2Third;
+    private Button q2a3;
 
     @FXML
-    private Button q3First;
+    private Button q3a1;
 
     @FXML
-    private Button q3Second;
+    private Button q3a2;
 
     @FXML
-    private Button q3Third;
+    private Button q3a3;
 
     @FXML
     private Label question1;
@@ -50,88 +46,23 @@ public class TestController {
     @FXML
     private Label question3;
 
-    private TestMediator testMediator= TestMediator.getInstance(); //mediatore per il test in generale
+    private TestMediator mediator = TestMediator.getInstance();
 
-    private QuestionsMediator mediator1 = new QuestionsMediator(); //mediatore per la prima domanda
-    private QuestionsMediator mediator2 = new QuestionsMediator(); //mediatore per la seconda domanda
-    private QuestionsMediator mediator3 = new QuestionsMediator(); //mediatore per la terza domanda
+    public void initialize() {
+        mediator.setButtons(q1a1, q1a2, q1a3, q2a1, q2a2, q2a3, q3a1, q3a2, q3a3, confirmationButton);
+        confirmationButton.setDisable(true); //il bottone di conferma è disabilitato finchè non si risponde a tutte le domande, il resto lo gestisce il mediatore
+    }
 
-    //init che mi disabilita in automatico il button di conferma, non cliccabile
-    void init()
-    {
-        testMediator.resetCounter();
-        confirmationButton.setDisable(true);
+
+    @FXML //click generico sui button
+    void buttonClick(ActionEvent event) {
 
     }
 
-    @FXML
-    void confirmationClick(ActionEvent event)
-    {
-        testMediator.verifyTest(); //da implementare
+    @FXML  //cosa succede se il button di conferma, abilitato dal mediatore, viene cliccato
+    void confirmationClick(ActionEvent event) {
+
 
     }
-
-    @FXML
-    void q1click1(ActionEvent event)
-    {
-        boolean answered = mediator1.controlling(1);
-        testMediator.buttonClicked(answered, q1First); //va a modificare il contatore del test in generale
-    }
-
-    @FXML
-    void q1click2(ActionEvent event) {
-        boolean answered = mediator1.controlling(2);
-        testMediator.buttonClicked(answered, q1Second); //va a modificare il contatore del test in generale
-
-    }
-
-    @FXML
-    void q1click3(ActionEvent event)
-    {
-        boolean answered = mediator1.controlling(3);
-        testMediator.buttonClicked(answered, q1Third); //va a modificare il contatore del test in generale
-    }
-
-    @FXML
-    void q2click1(ActionEvent event)
-    {
-        boolean answered = mediator2.controlling(1);
-        testMediator.buttonClicked(answered, q2First); //va a modificare il contatore del test in generale
-
-    }
-
-    @FXML
-    void q2click2(ActionEvent event) {
-        boolean answered = mediator2.controlling(2);
-        testMediator.buttonClicked(answered, q2Second); //va a modificare il contatore del test in generale
-
-    }
-
-    @FXML
-    void q2click3(ActionEvent event) {
-        boolean answered = mediator2.controlling(3);
-        testMediator.buttonClicked(answered, q2Third); //va a modificare il contatore del test in generale
-
-    }
-
-    @FXML
-    void q3click1(ActionEvent event) {
-        boolean answered = mediator3.controlling(1);
-        testMediator.buttonClicked(answered, q3First); //va a modificare il contatore del test in generale
-
-    }
-
-    @FXML
-    void q3click2(ActionEvent event) {
-        boolean answered = mediator3.controlling(2);
-        testMediator.buttonClicked(answered, q3Second); //va a modificare il contatore del test in generale
-    }
-
-    @FXML
-    void q3click3(ActionEvent event) {
-        boolean answered = mediator3.controlling(3);
-        testMediator.buttonClicked(answered, q3Third); //va a modificare il contatore del test in generale
-    }
-
 
 }
--- src/main/java/info/unical/Controller/TestMediator.java
+++ src/main/java/info/unical/Controller/TestMediator.java
@@ -1,14 +1,40 @@
 package info.unical.Controller;
 
 import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 
 public class TestMediator {
-    private static TestMediator instance = null;
-    private int buttonCounter = 0;
+    private static TestMediator instance = null; //singleton
+    private Button b1, b2, b3, b4, b5, b6, b7, b8, b9, confirmationButton; //bottoni delle risposte, il mediatore deve avere un riferimento
+
+    Map<Integer, Integer> map = new HashMap<>(); //mappa per le risposte
+    Map<Button, Integer> buttonToNumber = new HashMap<>();
+
+
+    private TestMediator() {}
+
+    void init()
+    {
+        buttonToNumber.put(b1, 1);
+        buttonToNumber.put(b2, 2);
+        buttonToNumber.put(b3, 3);
+        buttonToNumber.put(b4, 1);
+        buttonToNumber.put(b5, 2);
+        buttonToNumber.put(b6, 3);
+        buttonToNumber.put(b7, 1);
+        buttonToNumber.put(b8, 2);
+        buttonToNumber.put(b9, 3);
+
+    }
+
+
 
     public static TestMediator getInstance() {
         if (instance == null) {
@@ -17,48 +43,105 @@ public class TestMediator {
         return instance;
     }
 
-    public void incrementCounter()
-    {
-        buttonCounter++;
+    public void setButtons(Button b1, Button b2, Button b3, Button b4, Button b5, Button b6, Button b7, Button b8,
+            Button b9, Button ConfirmationButton) {
+        this.b1 = b1;
+        this.b2 = b2;
+        this.b3 = b3;
+        this.b4 = b4;
+        this.b5 = b5;
+        this.b6 = b6;
+        this.b7 = b7;
+        this.b8 = b8;
+        this.b9 = b9;
+        this.confirmationButton = confirmationButton;
     }
 
-    public void decrementCounter()
+    public void newClick(ActionEvent event)
     {
-        buttonCounter--;
-    }
-    public void resetCounter()
-    {
-        buttonCounter = 0;
-    }
+        Button buttonIJustClicked = (Button) event.getSource(); //mi prendo il bottone che ho cliccato dall'evento (il click)
+        int newAnswer = buttonToNumber.get(buttonIJustClicked); //mi prendo il valore associato al bottone che ho cliccato
 
-    public int getCounter()
-    {
-        return buttonCounter;
-    }
+        int question; // mi setto un valore per capire quale bottone è stato cliccato, in modo da capire a che domanda si sta rispondendo
 
-    public boolean isTestFinished()
-    {
-        return buttonCounter == 3;
-    }
-
-    public void buttonClicked(boolean alreadyClicked, Button confirmationButton)
-    {
-        if(alreadyClicked)
-        {
-            decrementCounter(); //se il button era selezionato e lo clicco nuovamente, decremento il counter (l'ho deselezionato)
-        }
+        if(buttonIJustClicked == b1 || buttonIJustClicked == b2 || buttonIJustClicked == b3)
+        { question = 1; }
+        else if(buttonIJustClicked == b4 || buttonIJustClicked == b5 || buttonIJustClicked == b6)
+        { question = 2; }
         else
+        { question = 3;}
+
+        if (map.containsKey(question))
         {
-            incrementCounter(); //al contrario, seleziono il button non cliccato prima e incremento il counter
+            //mi prendo il valore associato alla chiave question, quindi la risposta alla domanda
+            int oldValue = map.get(question);
+            if (newAnswer == oldValue) //se il valore associato al bottone che ho cliccato è uguale a quello che avevo già cliccato
+            {
+                map.remove(question); //rimuovo la risposta alla domanda
+                buttonIJustClicked.setStyle("-fx-background-color: #d75c00"); //cambio lo stile del bottone in modo da "deselezionarlo"
+                actionOnConfirm(); //controllo se sono state date tutte le risposte
+                return;
+            }
+
+            //mi prendo il bottone associato al valore vecchio e vado a cambiare lo stile in modo da "deselezionarlo"
+            switch (question)
+            {
+                case 1: //risposta alla domanda 1
+                    if (oldValue == 1) //il button scelto prima era 1
+                    { b1.setStyle("-fx-background-color: #d75c00"); }
+                    else if (oldValue == 2) //il button scelto prima era 2
+                    { b2.setStyle("-fx-background-color: #d75c00"); }
+                    else //il button scelto prima era 3
+                    { b3.setStyle("-fx-background-color: #d75c00"); }
+                    break;
+                case 2:
+                    if (oldValue == 1)
+                    { b4.setStyle("-fx-background-color: #d75c00"); }
+                    else if (oldValue == 2)
+                    { b5.setStyle("-fx-background-color: #d75c00");}
+                    else
+                    { b6.setStyle("-fx-background-color: #d75c00"); }
+                    break;
+                case 3:
+                    if (oldValue == 1) //il button scelto prima era 1 per la risposta 3
+                    { b7.setStyle("-fx-background-color: #d75c00"); }
+                    else if (oldValue == 2) //il button scelto prima era 2 per la risposta 3
+                    { b8.setStyle("-fx-background-color: #d75c00"); }
+                    else //il button scelto prima era 3 per la risposta 3
+                    { b9.setStyle("-fx-background-color: #d75c00"); }
+                    break;
+            }
+
+            map.replace(question, newAnswer); //aggiorno la risposta alla domanda
+        }
+
+        else //se non c'è già una risposta alla domanda
+        { map.put(question, newAnswer); //aggiungo la risposta alla domanda
         }
 
 
-        if (isTestFinished())
-        { confirmationButton.setDisable(false); }
-        else
-        { confirmationButton.setDisable(true); }
+        //cambio lo stile al button appena selezionato
+        buttonIJustClicked.setStyle("-fx-background-color: #ffffff"); //è cambiato lol
+        buttonIJustClicked.setStyle("-fx-border-color: #d75c00");
+        buttonIJustClicked.setStyle("-fx-text-fill: #d75c00");
+
+        actionOnConfirm(); //controllo se sono state date tutte le risposte
+
     }
 
-    public void verifyTest(){}; //da implementare
+    public boolean isReady() {
+        return map.size() == 3; //se la mappa ha 3 elementi, allora sono state date tutte le risposte
+    }
+
+    public void actionOnConfirm()
+    {
+        if (isReady())
+        {
+            confirmationButton.setDisable(false); //se sono state date tutte le risposte, abilito il bottone di conferma
+            return;
+        }
+        confirmationButton.setDisable(true); //altrimenti lo disabilito
+
+    }
 
 }
