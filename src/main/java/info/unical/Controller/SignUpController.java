89af413021861a1d62e0fabc29ba1ee580c23888
nika <129058062+nicolearnieri@users.noreply.github.com>
2023-12-17 17:18:03 +0100

    password fields carini

--- src/main/java/info/unical/Controller/SignUpController.java
+++ src/main/java/info/unical/Controller/SignUpController.java
@@ -4,11 +4,22 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.util.Objects;
 
 public class SignUpController {
 
     @FXML
     private TextField confirmPVisible;
+
+    @FXML
+    private ImageView image;
+
+
+    @FXML
+    private ImageView cImage;
 
     @FXML
     private PasswordField confirmPasswordField;
@@ -36,10 +47,55 @@ public class SignUpController {
 
     static ObservableList<String> languageList = FXCollections.observableArrayList("Inglese","Francese", "Spagnolo", "Portoghese");
 
+    Image openEye = new Image(Objects.requireNonNull(getClass().getResource("/images/open_eye.png")).toExternalForm());
+    Image eyeOff = new Image(Objects.requireNonNull(getClass().getResource("/images/eye_off.png")).toExternalForm());
 
-   public void init()
+
+
+    public void init()
    {
        languageChooser.setItems(languageList);
+       passwordFields();
 
    }
+
+   public void passwordFields()
+   {
+         passwordField.textProperty().bindBidirectional(seePField.textProperty());
+         confirmPasswordField.textProperty().bindBidirectional(confirmPVisible.textProperty());
+         image.setImage(openEye); //password
+         cImage.setImage(openEye); //confirmation password
+
+         image.setOnMouseClicked(event -> {
+              if (image.getImage() == openEye)
+              {
+                image.setImage(eyeOff);
+                passwordField.setVisible(false);
+                seePField.setVisible(true);
+              }
+              else
+              {
+                image.setImage(openEye);
+                seePField.setVisible(false);
+                passwordField.setVisible(true);
+              }
+         });
+
+         cImage.setOnMouseClicked(event -> {
+              if (cImage.getImage() == openEye)
+              {
+                cImage.setImage(eyeOff);
+                confirmPasswordField.setVisible(false);
+                confirmPVisible.setVisible(true);
+              }
+              else
+              {
+                cImage.setImage(openEye);
+                confirmPasswordField.setVisible(true);
+                confirmPVisible.setVisible(false);
+              }
+         });
+   }
+
+
 }
